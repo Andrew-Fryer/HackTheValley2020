cbc23eaf58fa3d83fe75d438697133dec88be840
Nathan Lai <39472876+Nathankhlai@users.noreply.github.com>
2020-02-15 15:21:08 -0500

    Add files via upload
    draft 1 of musicEditing

--- backend/musicEditing.py
+++ backend/musicEditing.py
@@ -1,1 +1,10 @@
 from pydub import AudioSegment
+
+chill = AudioSegment.from_file("../hackthevalley2020/backend/musicSamples/Foo1.wav", format = "wav")
+chill2 = AudioSegment.from_file("../hackthevalley2020/backend/musicSamples/Foo2.wav", format = "wav")
+
+beginning = chill
+end = chill2
+
+crossfade = (beginning.append(end, crossfade = 2000)).fade_in(2000).fade_out(3000)
+
